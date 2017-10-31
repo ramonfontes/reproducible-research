ed3a7a01b84ebc9bea96c5b02e0c97705cc2f7c6
Ramon Fontes <ramonreisfontes@gmail.com>
2017-10-31 10:08:05 -0300

    update code

--- mininet-wifi/ACROSS-Sweden-2017/tc_wmediumd.py
+++ mininet-wifi/ACROSS-Sweden-2017/tc_wmediumd.py
@@ -3,7 +3,7 @@
 'Starting a topology with and without wmediumd'
 
 from mininet.net import Mininet
-from mininet.node import Controller, OVSKernelAP
+from mininet.node import Controller, UserAP
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
@@ -13,22 +13,22 @@ def topology(wmediumd):
 
     "Create a network."
     if wmediumd:
-        net = Mininet(controller=Controller, link=TCLink, accessPoint=OVSKernelAP, enable_wmediumd=True, enable_interference=True)
+        net = Mininet(controller=Controller, link=TCLink, accessPoint=UserAP, enable_wmediumd=True, enable_interference=True)
     else:
-        net = Mininet(controller=Controller, link=TCLink, accessPoint=OVSKernelAP)
+        net = Mininet(controller=Controller, link=TCLink, accessPoint=UserAP)
 
     print "*** Creating nodes"
-    sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', ip='10.0.0.1/8', position='120,140,0')
-    sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', ip='10.0.0.2/8', position='10,30,0')
-    sta3 = net.addStation('sta3', mac='00:00:00:00:00:03', ip='10.0.0.3/8', position='10,50,0')
-    ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='g', channel='1', position='15,30,0', ieee80211r='yes', mobility_domain='a2b2')
+    sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', ip='10.0.0.1/8', position='120,140,0', range=10)
+    sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', ip='10.0.0.2/8', position='10,30,0', range=10)
+    sta3 = net.addStation('sta3', mac='00:00:00:00:00:03', ip='10.0.0.3/8', position='10,50,0', range=10)
+    ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='g', channel='1', position='15,30,0', ieee80211r='yes', mobility_domain='a1b2', passwd='123456789a', encrypt='wpa2', range=33)
     c1 = net.addController('c1', controller=Controller)
+
+    print "*** Configuring Propagation Model"
+    net.propagationModel("logDistancePropagationLossModel", exp = 3.7)
 
     print "*** Configuring wifi nodes"
     net.configureWifiNodes()
-
-    print "*** Configuring Propagation Model"
-    net.propagationModel("logDistancePropagationLossModel", exp = 3.5)
 
     'plotting graph'
     net.plotGraph(max_x=150, max_y=150)
