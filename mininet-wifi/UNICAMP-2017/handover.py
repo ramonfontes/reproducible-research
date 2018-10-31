aff385ec5f94227d3fd5a22fc34005b707962371
Ramon Fontes <ramonreisfontes@gmail.com>
2018-10-31 17:16:18 -0300

    Update handover.py

--- mininet-wifi/UNICAMP-2017/handover.py
+++ mininet-wifi/UNICAMP-2017/handover.py
@@ -2,52 +2,56 @@
 
 'Example for Handover'
 
-from mn_wifi.net import Mininet_wifi
-from mn_wifi.cli import CLI_wifi
+from mininet.node import Controller
 from mininet.log import setLogLevel, info
+from mn_wifi.cli import CLI_wifi
+from mn_wifi.net import Mininet_wifi
 
 
 def topology():
-
     "Create a network."
-    net = Mininet_wifi()
+    net = Mininet_wifi(controller=Controller)
 
     info("*** Creating nodes\n")
-    sta1 = net.addStation('sta1', mac='00:00:00:00:00:02', ip='10.0.0.2/8')
-    sta2 = net.addStation('sta2', mac='00:00:00:00:00:03', ip='10.0.0.3/8')
+    sta1 = net.addStation('sta1', mac='00:00:00:00:00:02', ip='10.0.0.2/8',
+                          range=20)
+    sta2 = net.addStation('sta2', mac='00:00:00:00:00:03', ip='10.0.0.3/8',
+                          range=20)
     ap1 = net.addAccessPoint('ap1', ssid='ssid-ap1', mode='g', channel='1',
-                             failMode='standalone', position='15,30,0')
+                             position='15,30,0', range=30)
     ap2 = net.addAccessPoint('ap2', ssid='ssid-ap2', mode='g', channel='6',
-                             failMode='standalone', position='55,30,0')
+                             position='55,30,0', range=30)
+    c1 = net.addController('c1')
 
-    net.setPropagationModel(model='logDistance', exp=5)
+    net.setPropagationModel(model="logDistance", exp=5)
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
 
     info("*** Creating links\n")
     net.addLink(ap1, ap2)
-    net.addLink(ap1, sta1)
-    net.addLink(ap1, sta2)
 
-    'plotting graph'
     net.plotGraph(max_x=100, max_y=100)
 
     net.startMobility(time=0)
     net.mobility(sta1, 'start', time=1, position='10,30,0')
     net.mobility(sta2, 'start', time=2, position='10,40,0')
-    net.mobility(sta1, 'stop', time=30, position='60,30,0')
+    net.mobility(sta1, 'stop', time=10, position='60,30,0')
     net.mobility(sta2, 'stop', time=10, position='25,40,0')
-    net.stopMobility(time=40)
+    net.stopMobility(time=11)
 
     info("*** Starting network\n")
     net.build()
+    c1.start()
+    ap1.start([c1])
+    ap2.start([c1])
 
     info("*** Running CLI\n")
     CLI_wifi(net)
 
     info("*** Stopping network\n")
     net.stop()
+
 
 if __name__ == '__main__':
     setLogLevel('info')
